e4c9298508840bf79ad2e9fe313856aff1cd1b0c
Christian Peraza <christianperaza2000@gmail.com>
2026-07-03 02:01:13 -0300

    Test getNotes

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import * as notesRoutes from '../services/notesRoutes';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.send('Index route');
+});
+
+router.get('/notes', (_req, res) => {
+  res.send(Buffer.from(JSON.stringify(notesRoutes.getNotes())));
 });
 
 router.post('/', (_req, res) => {
