878b360e825efbcf63e84134c12688c6c07eb8c2
Christian Peraza <christianperaza2000@gmail.com>
2026-07-03 00:21:09 -0300

    Edit index routes to simplify

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import noteRouter from './routes/notes';
+import indexRouter from './routes/indexRoutes';
 
 const app = express();
 app.use(express.json());
 const PORT = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Main route');
-});
+//app.get('/', (_req, res) => {
+//  res.send('Main route');
+//});
 
-app.use('/api/notes', noteRouter);
+app.use('/', indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all entry notes...');
+  res.send('Index route');
 });
 
 router.post('/', (_req, res) => {
